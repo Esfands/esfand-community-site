d829ace7af32011dfc68594adea9b796974dff59
Guacsenpai <guac@guacsenpai.com>
2023-01-18 14:29:57 -0600

    cleanup & separate gameserver components

--- components/gameserverpanel.tsx
+++ components/gameserverpanel.tsx
@@ -1,54 +1,25 @@
 import { CheckCircleIcon, InformationCircleIcon, LockClosedIcon, UserCircleIcon, ServerStackIcon, XCircleIcon } from '@heroicons/react/24/outline'
 
-const servers = [
-    {
-        
-        name: "Minecraft Server 1 Placeholder",
-        info: "Java Edition - Vanilla No Mods",
-        status: "Online",
-        players: "0/20",
-        serverIp: "mc.esfans.net:25566",
-        aaa: "",
-        art: "https://static-cdn.jtvnw.net/ttv-boxart/27471_IGDB-144x192.jpg"
-    },
-    {
-        
-        name: "Minecraft Server 2 Placeholder",
-        info: "Java Edition - Valhesia Mod",
-        status: "Offline",
-        players: "0/20",
-        serverIp: "Pending",
-        aaa: "",
-        art: "https://static-cdn.jtvnw.net/ttv-boxart/27471_IGDB-144x192.jpg"
-    },
-    {
-        
-        name: "Terraria Server 1 Placeholder",
-        info: "Vanilla - No Mods",
-        status: "Online",
-        players: "0/20",
-        serverIp: "tr.esfans.net",
-        aaa: "esfand",
-        art: "https://static-cdn.jtvnw.net/ttv-boxart/31376_IGDB-144x192.jpg"
-    },
-    {
-        
-        name: "Valheim Server 1 Placeholder",
-        info: "Vanilla - No Mods",
-        status: "Offline",
-        players: "0/20",
-        serverIp: "Pending",
-        aaa: "",
-        art: "https://static-cdn.jtvnw.net/ttv-boxart/508455_IGDB-144x192.jpg"
-    },
-  ]
+interface Props {
+    server: Server
+}
 
-  function getStatus(serverStatus: string) {
-    if (serverStatus == "Online") {
+interface Server {
+    name: string,
+    info: string,
+    status: boolean,
+    players: string,
+    address: string,
+    aaa: string,
+    art: string
+  }
+
+  function getStatus(serverStatus: boolean) {
+    if (serverStatus) {
         return (
             <p className="mt-2 flex items-center text-sm text-gray-500">
                 <CheckCircleIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-green-400" aria-hidden="true" />
-                {serverStatus}
+                Online
             </p>
         );
     }
@@ -56,7 +27,7 @@ const servers = [
     return (
         <p className="mt-2 flex items-center text-sm text-gray-500">
             <XCircleIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-red-600" aria-hidden="true" />
-            {serverStatus}
+            Offline
         </p>
     );
     
@@ -73,47 +44,41 @@ const servers = [
     }
   }
 
-export const GameServerPanel = () => {
+export const GameServerPanel = ({ server } : Props) => {
     return (
-        <div className="overflow-hidden bg-white shadow sm:rounded-md">
-      <ul role="list" className="divide-y divide-gray-200">
-        {servers.map((server) => (
-          <li key={server.name}>
+        <li key={server.name}>
             <div className="block">
-              <div className="flex items-center px-4 py-4 sm:px-6">
-                <div className="flex min-w-0 flex-1 items-center">
-                  <div className="flex-shrink-0">
-                    <img className="h-48 w-36" src={server.art} alt="" />
-                  </div>
-                  <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
-                    <div>
-                      <p className="truncate text-sm font-medium text-indigo-600">{server.name}</p>
-                      <p className="mt-2 flex items-center text-sm text-gray-500">
-                        <InformationCircleIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                        <span className="truncate">{server.info}</span>
-                      </p>
+                <div className="flex items-center px-4 py-4 sm:px-6">
+                    <div className="flex min-w-0 flex-1 items-center">
+                        <div className="flex-shrink-0">
+                            <img className="h-48 w-36" src={server.art} alt="" />
+                        </div>
+                    <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
+                        <div>
+                            <p className="truncate text-sm font-medium text-indigo-600">{server.name}</p>
+                            <p className="mt-2 flex items-center text-sm text-gray-500">
+                                <InformationCircleIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                                <span className="truncate">{server.info}</span>
+                            </p>
+                        </div>
+                        <div className="hidden md:block">
+                            <div>
+                                {getStatus(server.status)}
+                                <p className="mt-2 flex items-center text-sm text-gray-500">
+                                    <ServerStackIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                                    {server.address}
+                                </p>
+                                {getAAA(server.aaa)}
+                                <p className="mt-2 flex items-center text-sm text-gray-500">
+                                    <UserCircleIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                                    Players {server.players}
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="hidden md:block">
-                      <div>
-                        {getStatus(server.status)}
-                        <p className="mt-2 flex items-center text-sm text-gray-500">
-                            <ServerStackIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                            {server.serverIp}
-                        </p>
-                        {getAAA(server.aaa)}
-                        <p className="mt-2 flex items-center text-sm text-gray-500">
-                            <UserCircleIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                            Players {server.players}
-                        </p>
-                      </div>
-                    </div>
-                  </div>
                 </div>
-              </div>
             </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+        </div>
+    </li>
     );
 }
--- pages/community/gameservers.tsx
+++ pages/community/gameservers.tsx
@@ -1,14 +1,71 @@
 import { GameServerPanel } from "../../components/gameserverpanel";
 
+interface Server {
+  name: string,
+  info: string,
+  status: boolean,
+  players: string,
+  address: string,
+  aaa: string,
+  art: string
+}
+
+const servers : Server[] = [
+  {
+      
+      name: "Minecraft Server 1 Placeholder",
+      info: "Java Edition - Vanilla No Mods",
+      status: true,
+      players: "0/20",
+      address: "mc.esfans.net:25566",
+      aaa: "",
+      art: "https://static-cdn.jtvnw.net/ttv-boxart/27471_IGDB-144x192.jpg"
+  },
+  {
+      
+      name: "Minecraft Server 2 Placeholder",
+      info: "Java Edition - Valhesia Mod",
+      status: false,
+      players: "0/20",
+      address: "Pending",
+      aaa: "",
+      art: "https://static-cdn.jtvnw.net/ttv-boxart/27471_IGDB-144x192.jpg"
+  },
+  {
+      
+      name: "Terraria Server 1 Placeholder",
+      info: "Vanilla - No Mods",
+      status: true,
+      players: "0/20",
+      address: "tr.esfans.net",
+      aaa: "esfand",
+      art: "https://static-cdn.jtvnw.net/ttv-boxart/31376_IGDB-144x192.jpg"
+  },
+  {
+      
+      name: "Valheim Server 1 Placeholder",
+      info: "Vanilla - No Mods",
+      status: false,
+      players: "0/20",
+      address: "Pending",
+      aaa: "",
+      art: "https://static-cdn.jtvnw.net/ttv-boxart/508455_IGDB-144x192.jpg"
+  },
+]
+
 export default function Gameservers() {
     return(
       <main className="-mt-24">
       <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-        {/* Replace with your content */}
         <div className="px-4 py-8 sm:px-0">
-          <GameServerPanel />
+          <div className="overflow-hidden bg-white shadow sm:rounded-md">
+            <ul role="list" className="divide-y divide-gray-200">
+              {servers.map((server) => (
+                <GameServerPanel server={server}/>
+              ))}
+            </ul>
+          </div>
         </div>
-        {/* /End replace */}
       </div>
     </main>
     )
