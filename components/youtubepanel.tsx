3ef7c373f7f0a18935cc5f073791cf9388e0d86e
Guacsenpai <guac@guacsenpai.com>
2023-01-27 02:58:19 -0600

    get latest youtube video from props - add safety checks later

--- components/youtubepanel.tsx
+++ components/youtubepanel.tsx
@@ -1,11 +1,16 @@
-export const YoutubePanel = () => {
+export const YoutubePanel = ({ video }) => {
+
+    function getVideoId() {
+        return `https://www.youtube.com/embed/${video.data[0].videoData.items[0].snippet.resourceId.videoId}`;
+    }
+
     return (
         <div className="rounded-lg bg-[#1b2126] shadow h-100">
             <iframe
                 className='aspect-video'
                 width={'100%'} 
                 height={'100%'}
-                src="https://www.youtube.com/embed/ifvkWc9Y9DU" 
+                src={getVideoId()} 
                 title="YouTube video player" 
                 frameborder="0" 
                 allow="accelerometer; 
