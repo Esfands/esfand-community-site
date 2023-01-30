88ed0468e0d9823013828de302bedcc482fcb416
mahcks <maxnatural13@gmail.com>
2023-01-29 23:26:45 -0600

    corners stay rounded regardless of view

--- pages/community/gameservers.tsx
+++ pages/community/gameservers.tsx
@@ -54,7 +54,7 @@ export default function Gameservers() {
     <main className="-mt-24">
       <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="px-4 py-8 sm:px-0">
-          <div className="overflow-hidden bg-white shadow sm:rounded-md">
+          <div className="overflow-hidden bg-white shadow rounded-lg">
             <ul role="list" className="divide-y divide-gray-200">
               {servers.map((server) => (
                 <GameServerPanel server={server} key={server.name} />
