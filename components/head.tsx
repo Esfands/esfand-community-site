577d4fbb2a5e77fdf8ab4ca6b81b66130dc1db34
mahcks <maxnatural13@gmail.com>
2023-02-02 21:23:56 -0600

    updated ECS to ESF in tab title

--- components/head.tsx
+++ components/head.tsx
@@ -3,7 +3,7 @@ import NextHead from "next/head";
 export default function Head(tab: string) {
   return (
     <NextHead>
-      <title>{`ECS | ${tab}`}</title>
+      <title>{`ESF | ${tab}`}</title>
       <meta name="description" content="" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
