6e97a6c95e2077a6c94aa9611eb779297ad094dd
Guacsenpai <guac@guacsenpai.com>
2023-02-04 14:48:30 -0600

    start discord unban form

--- pages/community/discordunban.tsx
+++ pages/community/discordunban.tsx
@@ -5,10 +5,70 @@ export default function DiscordUnban() {
     <>
       {Head("Discord Unban Request")}
 
-      <main className="h-screen -mt-24 pb-8">
-        <h1 className="text-white font-bold text-2xl text-center">
-          Discord Unban Request
-        </h1>
+      <main className="-mt-24">
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="px-4 py-8 sm:px-0">
+            <div className="overflow-hidden bg-[#1b2126] shadow rounded-lg">
+              <form className="space-y-8 divide-y divide-gray-200">
+                <div className="space-y-8 divide-y divide-gray-200">
+                  <div>
+                    <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+                      <div className="sm:col-span-4">
+                        <label htmlFor="username" className="block text-sm font-medium text-white">
+                          Discord Tag
+                        </label>
+                        <div className="mt-1 flex rounded-md shadow-sm">
+                          <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-gray-500 sm:text-sm">
+                            Discord Tag
+                          </span>
+                          <input
+                            type="text"
+                            name="username"
+                            id="username"
+                            autoComplete="username"
+                            className="block w-full min-w-0 flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                          />
+                        </div>
+                      </div>
+                      <div className="sm:col-span-3">
+                        <label htmlFor="country" className="block text-sm font-medium text-white">
+                          Country
+                        </label>
+                        <div className="mt-1">
+                          <select
+                            id="country"
+                            name="country"
+                            autoComplete="country-name"
+                            className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                          >
+                            <option>United States</option>
+                            <option>Canada</option>
+                            <option>Mexico</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="sm:col-span-6">
+                  <label htmlFor="about" className="block text-sm font-medium text-white">
+                    About
+                  </label>
+                  <div className="mt-1">
+                    <textarea
+                      id="about"
+                      name="about"
+                      rows={3}
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      defaultValue={''}
+                    />
+                  </div>
+                  <p className="mt-2 text-sm text-gray-500">Write a few sentences about yourself.</p>
+                </div>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
