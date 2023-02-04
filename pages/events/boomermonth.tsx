ed45426d48e7e94fa03b014f50a1e6a7e000ee5f
Guacsenpai <guac@guacsenpai.com>
2023-02-04 14:56:32 -0600

    add boomer month placeholder

--- pages/events/boomermonth.tsx
+++ pages/events/boomermonth.tsx
@@ -2,8 +2,17 @@ import Head from "../../components/head";
 
 export default function BoomerMonth() {
     return (
-      <>
-        {Head("Boomer Month")}
-      </>
+        <>
+            {Head("Boomer Month")}
+            <main className="-mt-24">
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="px-4 py-8 sm:px-0">
+                        <div className="overflow-hidden bg-[#1b2126] shadow rounded-lg">
+                            <p className="text-white pb-96">Filler Content</p> 
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
     );
 }
