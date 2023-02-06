accc8ecb6baa7fd60929fcac2aeb1a41ec7b840b
mahcks <maxnatural13@gmail.com>
2023-02-05 19:42:03 -0600

    simplified regex

--- utils/index.tsx
+++ utils/index.tsx
@@ -5,11 +5,9 @@ export function classNames(...classes: string[]) {
 // Takes in a video URL and parses out the video id and returns a thumbnail.
 // If no ID found by regex, it gives the default thumbnail
 export function getThumbnail(videoUrl: string) {
-  const res = videoUrl.match(
-    /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/
-  );
+  const res = videoUrl.match(/v=([^&]+)/);
 
-  const videoId = res ? res[1] : 'unknown_image';
+  const videoId = res ? res[1] : "unknown_image";
 
-  return `https://img.youtube.com/vi/${videoId}/mqdefault.jpg`
+  return `https://img.youtube.com/vi/${videoId}/mqdefault.jpg`;
 }
