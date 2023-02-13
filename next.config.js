33b1a278264e94e60da132937824c2509d1f7b29
Guacsenpai <guac@guacsenpai.com>
2023-02-13 15:47:39 -0600

    update next config for cors?

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  typescript: { ignoreBuildErrors: true }
+  typescript: { ignoreBuildErrors: true },
+  headers: [
+    { key: "Access-Control-Allow-Origin", value: "https://esfans.net"},
+  ]
 }
 
 module.exports = nextConfig
