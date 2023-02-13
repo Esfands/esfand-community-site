e9f4136dc72f05259053e64779c42dfe18ca503c
Guacsenpai <guac@guacsenpai.com>
2023-02-13 15:35:09 -0600

    test env

--- pages/api/unban.ts
+++ pages/api/unban.ts
@@ -3,5 +3,5 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export default function Unban(req: NextApiRequest, res: NextApiResponse) {
   
   const apiKey = process.env.API_KEY;
-  res.status(200).json({ name: 'placeholder' })
+  res.status(200).json({ name: apiKey?.substring(0, 3) })
 }
