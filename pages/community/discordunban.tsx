2ebb693a973e45abcf8006906ef0c364b70bc3d1
Guacsenpai <guac@guacsenpai.com>
2023-02-13 15:59:16 -0600

    crippling pain www

--- pages/community/discordunban.tsx
+++ pages/community/discordunban.tsx
@@ -35,7 +35,7 @@ export default function DiscordUnban() {
       body: jsonData,
     }
 
-    const response = await fetch(`https://esfans.net/api/unban/`, options);
+    const response = await fetch(`https://www.esfans.net/api/unban/`, options);
     const result = await response.json();
   }
 
