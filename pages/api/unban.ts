e918c1ee29626598b9647db675399cb7f69e8da4
Guacsenpai <guac@guacsenpai.com>
2023-02-17 03:46:02 -0600

    update unban req. add captcha req

--- pages/api/unban.ts
+++ pages/api/unban.ts
@@ -3,5 +3,87 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export default function Unban(req: NextApiRequest, res: NextApiResponse) {
   
   const apiKey = process.env.API_KEY;
-  res.status(200).json({ name: apiKey?.substring(0, 3) })
+  const captchaSecret = process.env.CAPTCHA_SECRET;
+  const emailRegex = new RegExp('/^[^\s@]+@[^\s@]+\.[^\s@]+$/');
+  const discordRegex = new RegExp('^.{3,32}#[0-9]{4}$');
+
+  const verifyCaptcha = async () => {
+
+    const data = {
+      secret: captchaSecret,
+      response: req.body.token,
+    }
+
+    const formBody = Object.keys(data).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])).join('&');
+    const options = {
+      method: 'POST',
+      mode: 'cors' as RequestMode,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: formBody
+    }
+
+    const response = await fetch(`https://www.google.com/recaptcha/api/siteverify`, options);
+    const result = await response.json();
+
+    if (!result.success)
+      return false;
+
+    return true;
+  }
+
+  const submitUnbanRequest = async () => {
+    const data = {
+
+    }
+    const options = {
+      method: 'POST',
+      mode: 'cors' as RequestMode,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'x-api-key': apiKey as string
+      },
+      body: JSON.stringify(data)
+    }
+
+    const response = await fetch(`https://mod.esfans.net/api/discord/unbans/esfandtv`, options);
+    const status = response.status;
+    
+    if (status == 200 || status == 201)
+      return true;
+    else
+      return false;
+  }
+
+  let success = false;
+  verifyCaptcha().then(result => {
+    success = result;
+  });
+
+  if (!success)
+    res.status(500).json({ message: 'An error occurred when submitting the unban request. Try again later or contact a moderator.'})
+  
+  // check for valid data
+  const reqData = req.body;
+  if (reqData.email == null || reqData.email == '' || reqData.email == undefined || reqData.discordTag == null || reqData.discordTag == '' || reqData.discordTag == undefined
+      || reqData.banRange == null || reqData.banRange == '' || reqData.banRange == undefined || reqData.reason == null || reqData.reason == '' || reqData.reason == undefined) {
+        res.status(400).json({ message: 'Missing request data'});
+      }
+
+  // regex email and username
+  if (!emailRegex.test(reqData.email))
+    res.status(400).json({ message: 'An invalid email was entered in the request. Try another email or contact a moderator.'});
+
+  if (!discordRegex.test(reqData.discordTag))
+    res.status(400).json({ message: 'An invalid username was entered in the request. Include the 4 digit identifier or contact a moderator.'});
+  
+    submitUnbanRequest().then(result => {
+      success = result;
+    })
+
+    if (success)
+      res.status(201);
+    else
+      res.status(500);
 }
--- pages/community/discordunban.tsx
+++ pages/community/discordunban.tsx
@@ -1,29 +1,40 @@
 import { Disclosure } from '@headlessui/react'
 import { MinusSmallIcon, PlusSmallIcon, BellAlertIcon, CloudArrowDownIcon, CodeBracketIcon } from '@heroicons/react/24/outline'
+import { useEffect } from 'react';
 import Head from "../../components/head";
+import ReactRecaptcha3 from 'react-google-recaptcha3';
+import { redirect } from 'next/dist/server/api-utils';
 
 const faqs = [
   {
-      question: "Question 1 Placeholder",
-      answer: "Answer 1 Placeholder",
+    question: "Question 1 Placeholder",
+    answer: "Answer 1 Placeholder",
   },
   {
     question: "Question 2 Placeholder",
     answer: "Answer 2 Placeholder",
   },
-  
+
 ]
 
 export default function DiscordUnban() {
 
+  useEffect(() => {
+    ReactRecaptcha3.init('6Le0aYokAAAAAEzHszvvm2StMrZACUoFMkINAiq2');
+  }, []);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log('yo');
+
     const data = {
       discordTag: event.target.discordTag.value,
+      email: event.target.email.value,
       banRange: event.target.banRange.value,
       reason: event.target.reason.value,
     }
+
+    const token = await ReactRecaptcha3.getToken();
+    data.token = token;
 
     const jsonData = JSON.stringify(data);
     const options = {
@@ -35,28 +46,39 @@ export default function DiscordUnban() {
       body: jsonData,
     }
 
-    const response = await fetch(`https://www.esfans.net/api/unban/`, options);
+    const response = await fetch(`https://esfans.net/api/unban/`, options);
     const result = await response.json();
   }
 
   return (
     <>
+      
       {Head("Discord Unban Request")}
-
+      
       <main className="-mt-24">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="px-4 py-8 sm:px-0">
             <div className="overflow-hidden bg-[#1b2126] shadow rounded-lg">
               <div className="flex justify-center text-white pt-4 text-2xl font-bold leading-10 tracking-tight">Discord Unban Request</div>
+              <div className="relative flex justify-center">
+              <div className="px-5 text-sm text-white ">
+                  This page is protected by reCAPTCHA and the Google
+                  <a href="https://policies.google.com/privacy" className="text-red-700 hover:text-red-600">&nbsp;Privacy Policy</a> and
+                  <a href="https://policies.google.com/terms" className="text-red-700 hover:text-red-600">&nbsp;Terms of Service</a> apply.
+                </div>
+              </div>
+              <div className="justify-center text-white tracking-tight">
+                
+              </div>
               <form onSubmit={handleSubmit} className="space-y-8 divide-y divide-gray-200">
                 <div className="space-y-8 divide-y divide-gray-200">
                   <div>
-                    <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6 justify-items-center">
-                      <div className="sm:col-span-6">
+                    <div className="mt-6 grid gap-y-6 grid-cols-12 md:px-52">
+                      <div className="col-span-12">
                         <label htmlFor="discordTag" className="block text-sm font-medium text-white">
                           Discord Tag
                         </label>
-                        <div className="mt-1 flex rounded-md shadow-sm">
+                        <div className="mt-1 flex rounded-md shadow-sm w-1/2 ">
                           <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-gray-500 sm:text-sm">
                             Discord Tag
                           </span>
@@ -65,11 +87,28 @@ export default function DiscordUnban() {
                             name="discordTag"
                             id="discordTag"
                             required
-                            className="block w-full min-w-0 flex-1 rounded-none rounded-r-md border-gray-300 sm:text-sm"
+                            className="block flex-1 rounded-none rounded-r-md border-gray-300 sm:text-sm"
                           />
                         </div>
                       </div>
-                      <div className="sm:col-span-6">
+                      <div className="col-span-12">
+                        <label htmlFor="email" className="block text-sm font-medium text-white">
+                          Email
+                        </label>
+                        <div className="mt-1 flex rounded-md shadow-sm w-1/2">
+                          <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-gray-500 sm:text-sm">
+                            Email
+                          </span>
+                          <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            required
+                            className="block flex-1 rounded-none rounded-r-md border-gray-300 sm:text-sm"
+                          />
+                        </div>
+                      </div>
+                      <div className="col-span-12">
                         <label htmlFor="banRange" className="block text-sm font-medium text-white">
                           When were you banned?
                         </label>
@@ -77,7 +116,8 @@ export default function DiscordUnban() {
                           <select
                             id="banRange"
                             name="banRange"
-                            className="block w-full rounded-md border-gray-300 shadow-sm sm:text-sm"
+                            required
+                            className="block rounded-md border-gray-300 shadow-sm sm:text-sm w-1/2"
                           >
                             <option>Less than 1 month ago</option>
                             <option>Less than 6 months ago</option>
@@ -86,7 +126,7 @@ export default function DiscordUnban() {
                           </select>
                         </div>
                       </div>
-                      <div className="sm:col-span-6">
+                      <div className="col-span-12">
                         <label htmlFor="reason" className="block text-sm font-medium text-white">
                           Reason you should be unbanned
                         </label>
@@ -95,14 +135,15 @@ export default function DiscordUnban() {
                             id="reason"
                             name="reason"
                             rows={3}
+                            required
                             className="block w-full rounded-md border-gray-300 shadow-sm sm:text-sm"
                             defaultValue={''}
                           />
                         </div>
                         <p className="mt-2 text-sm text-gray-500">Write a few sentences about why you should be unbanned.</p>
                       </div>
-                      <div className="sm:col-span-6 pb-5">
-                        <button type="submit" className="inline-flex items-center rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2">
+                      <div className="col-span-12 pb-5 grid justify-center">
+                        <button type="submit" className="rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2">
                           Request Unban
                         </button>
                       </div>
@@ -111,42 +152,42 @@ export default function DiscordUnban() {
                 </div>
               </form>
               <div className="relative">
-                        <div className="absolute inset-0 flex items-center" aria-hidden="true">
-                            <div className="w-full border-t border-gray-500" />
-                        </div>
-                    </div>
-                    <div className="text-white pb-7">
-                        <div className="mx-auto max-w-7xl px-6 pt-10 lg:px-8">
-                            <div className="mx-auto max-w-7xl divide-y divide-gray-300/10">
-                                <h2 className="text-2xl font-bold leading-10 tracking-tight flex justify-center">Frequently asked questions</h2>
-                                <dl className="mt-10 space-y-6 divide-y divide-gray-300/10">
-                                    {faqs.map((faq) => (
-                                        <Disclosure as="div" key={faq.question} className="pt-6">
-                                            {({ open }) => (
-                                                <>
-                                                    <dt>
-                                                        <Disclosure.Button className="flex w-full items-start justify-between text-left">
-                                                            <span className="text-base font-semibold leading-7">{faq.question}</span>
-                                                            <span className="ml-6 flex h-7 items-center">
-                                                                {open ? (
-                                                                    <MinusSmallIcon className="h-6 w-6" aria-hidden="true" />
-                                                                ) : (
-                                                                    <PlusSmallIcon className="h-6 w-6" aria-hidden="true" />
-                                                                )}
-                                                            </span>
-                                                        </Disclosure.Button>
-                                                    </dt>
-                                                    <Disclosure.Panel as="dd" className="mt-2 pr-12">
-                                                        <p className="text-base leading-7 text-gray-400">{faq.answer}</p>
-                                                    </Disclosure.Panel>
-                                                </>
-                                            )}
-                                        </Disclosure>
-                                    ))}
-                                </dl>
-                            </div>
-                        </div>
-                    </div>
+                <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                  <div className="w-full border-t border-gray-500" />
+                </div>
+              </div>
+              <div className="text-white pb-7">
+                <div className="mx-auto max-w-7xl px-6 pt-10 lg:px-8">
+                  <div className="mx-auto max-w-7xl divide-y divide-gray-300/10">
+                    <h2 className="text-2xl font-bold leading-10 tracking-tight flex justify-center">Frequently asked questions</h2>
+                    <dl className="mt-10 space-y-6 divide-y divide-gray-300/10">
+                      {faqs.map((faq) => (
+                        <Disclosure as="div" key={faq.question} className="pt-6">
+                          {({ open }) => (
+                            <>
+                              <dt>
+                                <Disclosure.Button className="flex w-full items-start justify-between text-left">
+                                  <span className="text-base font-semibold leading-7">{faq.question}</span>
+                                  <span className="ml-6 flex h-7 items-center">
+                                    {open ? (
+                                      <MinusSmallIcon className="h-6 w-6" aria-hidden="true" />
+                                    ) : (
+                                      <PlusSmallIcon className="h-6 w-6" aria-hidden="true" />
+                                    )}
+                                  </span>
+                                </Disclosure.Button>
+                              </dt>
+                              <Disclosure.Panel as="dd" className="mt-2 pr-12">
+                                <p className="text-base leading-7 text-gray-400">{faq.answer}</p>
+                              </Disclosure.Panel>
+                            </>
+                          )}
+                        </Disclosure>
+                      ))}
+                    </dl>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
